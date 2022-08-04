06f26f823fe3f99635ff2d35ee7def95c13053b7
Sky DeGrate <skydegrate@gmail.com>
2022-08-04 15:40:41 -0400

    finished

--- index.js
+++ index.js
@@ -18,7 +18,8 @@ darkLightButton.addEventListener('mouseout', (e) => {
 fetch(url)
 .then(res=> res.json())
 .then(results => {
-    let firstTenMovies = results.results.splice(0, 10)
+    let firstTenMovies = results.results
+    // .splice(0, 10)
     firstTenMovies.forEach(movie => {
         renderMovie(movie)
     })
@@ -29,19 +30,18 @@ let grid = document.getElementById('grid')
 const renderMovie = (movie) => {
    let div = document.createElement('div')
    div.classList.add('column')
-   div.innerText = `${movie.title} \nRating: ${movie.rated} Score: ${movie.score}`
-
-
    //creating an image element 
    let image = document.createElement('img')
+   image.className = "images";
    image.src = movie.image_url
-    div.append(image)
+   const movieDescription = document.createElement('p')
+   movieDescription.innerText = `${movie.title} \nRating: ${movie.rated} \nScore: ${movie.score}`
+    // div.innerText = `${movie.title} \nRating: ${movie.rated} \nScore: ${movie.score}`
+    div.append(image, movieDescription)
     grid.append(div)
-    
    // appending to .css hover card effect
 //    let bestCard = document.getElementsByClassName("card-div")
-//    bestCard.append(div)
-
+//    bestCard.append(div
 }
 const form = document.querySelector('#form')
 form.addEventListener('submit', (e) => {
